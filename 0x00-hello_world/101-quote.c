677f62a71e26c78768cb862e5716e0c660d89819
cakesofpan <gatibange@gmail.com>
2023-05-18 12:36:49 -0700

    Changes 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,5 +7,5 @@
  */
 int main(void)
 {
-	return(write(59, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 1));
+	return(write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59));
 }
