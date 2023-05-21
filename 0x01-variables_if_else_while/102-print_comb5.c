dd36ccc30a4983626d3b5433b4651d663af31aa1
cakesofpan <gatibange@gmail.com>
2023-05-21 15:19:10 -0700

    Changes 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,3 +7,22 @@
  */
 int main(void)
 {
+	int m = 0, t;
+	
+	for (; m <= 8; m++)
+	{
+		for (t = m + 1; t <= 9; t++)
+		{
+			putchar(m + '0');
+			putchar(t + '0');
+			
+			if (m != 8 || t != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
