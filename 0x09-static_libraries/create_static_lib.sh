7f83fba8e09e7fcc85911fb26febfbc8b6e018cf
cakesofpan <gatibange@gmail.com>
2023-06-14 20:08:55 -0700

    Changes 2

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ar -rc liball.a *.c
+ar -rc liball.a 0-isupper 0-memset 0-strcat 100-atoi 1-isdigit 1-memcpy 1-strncat 2-strchr 2-strlen 2-strncpy 3-islower 3-puts 3-strcmp 3-strspn 4-isalpha 4-strpbrk 5-strstr 6-abs 9-strcpy _putchar source_0 source_1 source_2
