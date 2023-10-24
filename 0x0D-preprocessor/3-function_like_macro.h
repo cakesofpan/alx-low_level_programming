b6215dd921f3389ce82784c3e7ce567aab645b38
cakesofpan <126176539+cakesofpan@users.noreply.github.com>
2023-10-25 01:25:00 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-extern int x;
-#define ABS(X) ((x) < 0 ? (-(x)) : (x))
-
-typedef int youmakeme;
-
-#endif /* FUNCTION_LIKE_MACRO_H */
