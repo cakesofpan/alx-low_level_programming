d26e3cedf9df07d1d4be337a93c383f020fd0a02
cakesofpan <gatibange@gmail.com>
2023-11-09 11:00:37 -0800

    Changes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(num, int));
 		if (t < n - 1)
-			printf("%d", va_arg(num, int));
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(num);
