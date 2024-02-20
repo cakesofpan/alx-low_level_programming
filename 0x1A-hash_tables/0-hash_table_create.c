252f7549b923a635d74622d44c63117ad31ff175
cakesofpan <gatibange@gmail.com>
2024-02-20 14:13:04 -0800

    C Hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	newhash->size = size;
-	newhash->array = malloc(sizeof(hash_node_t *) *size);
+	newhash->array = malloc(sizeof(hash_node_t * ) *size);
 	if (newhash->array == NULL)
 		return (NULL);
 
